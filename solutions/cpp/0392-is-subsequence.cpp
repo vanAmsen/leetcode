d6a99bca930779ad540e02aded5277ead8c22c84
Edgar <edgar.amsen@gmail.com>
2023-07-28 00:55:05 +0200

    Create 0392-is-subsequence.cpp

---
+++ solutions/cpp/0392-is-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j = 0;
+        for (int i = 0; i < t.size() && j < s.size(); i++) {
+            if (t[i] == s[j]) {
+                j++;
+            }
+        }
+        return j == s.size();
+    }
+};
