6b10c7e7dfe538d85d3e2a008f5e7c4237bddba2
Edgar <edgar.amsen@gmail.com>
2023-07-28 00:56:17 +0200

    Create 0392-is-subsequence.java

---
+++ solutions/java/0392-is-subsequence.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j = 0;
+        for (int i = 0; i < t.length() && j < s.length(); i++) {
+            if (t.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+        }
+        return j == s.length();
+    }
+}
