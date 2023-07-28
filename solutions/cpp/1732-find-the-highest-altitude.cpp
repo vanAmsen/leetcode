86eb5203d959514bf4b42042cdb8116f13be895a
Edgar <edgar.amsen@gmail.com>
2023-07-28 23:30:51 +0200

    Create 1732-find-the-highest-altitude.cpp

---
+++ solutions/cpp/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int max_altitude = 0;
+        int current_altitude = 0;
+        for(int g : gain){
+            current_altitude += g;
+            max_altitude = max(current_altitude, max_altitude);
+        }
+        return max_altitude;
+    }
+};
