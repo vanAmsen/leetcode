ba83ab7baeff3ec8720dec1b5fcd9e0587bcddb6
Edgar <edgar.amsen@gmail.com>
2023-07-28 02:36:46 +0200

    Create 486-predict-the-winner.py

---
+++ solutions/python/486-predict-the-winner.py
@@ -0,0 +1,12 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums) 
+        dp = [[0]*n for _ in range(n)] 
+        for i in range(n-1, -1, -1): 
+            for j in range(i, n): 
+                if i == j: 
+                    dp[i][j] = nums[i] 
+                else: 
+                    dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1])    
+        return dp[0][n-1] >= 0 
+                                                                                                                                   
