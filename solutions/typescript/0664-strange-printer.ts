61d09319d58c27b18a0a8d1fdc359afb1f8e1d35
Edgar <edgar.amsen@gmail.com>
2023-07-30 03:31:33 +0200

    Create 0664-strange-printer.ts

---
+++ solutions/typescript/0664-strange-printer.ts
@@ -0,0 +1,18 @@
+function strangePrinter(s: string): number {
+    const n = s.length;
+    const dp: number[][] = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i][i] = 1;
+        for (let j = i + 1; j < n; j++) {
+            dp[i][j] = dp[i][j - 1] + 1;
+            for (let k = i; k < j; k++) {
+                if (s[k] === s[j]) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + (k+1<=j-1 ? dp[k+1][j-1] : 0));
+                }
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+};
