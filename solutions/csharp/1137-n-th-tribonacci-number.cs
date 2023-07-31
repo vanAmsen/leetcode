90903aa83ad38cdaebada888b3c40d7e694cbcfa
Edgar <edgar.amsen@gmail.com>
2023-07-31 17:07:02 +0200

    Create 1137-n-th-tribonacci-number.cs

---
+++ solutions/csharp/1137-n-th-tribonacci-number.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int Tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        int a = 0, b = 1, c = 1;
+        for (int i = 0; i < n - 2; i++) {
+            int temp = a + b + c;
+            a = b;
+            b = c;
+            c = temp;
+        }
+        return c;
+    }
+}
