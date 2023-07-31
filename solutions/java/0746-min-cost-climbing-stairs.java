3d043d40576b694d8ed7910b6863e6702dbcf047
Edgar <edgar.amsen@gmail.com>
2023-07-31 17:27:15 +0200

    Create 0746-min-cost-climbing-stairs.java

---
+++ solutions/java/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int f1 = 0, f2 = 0;
+        for (int i = cost.length - 1; i >= 0; --i) {
+            int f0 = cost[i] + Math.min(f1, f2);
+            f2 = f1;
+            f1 = f0;
+        }
+        return Math.min(f1, f2);
+    }
+}
