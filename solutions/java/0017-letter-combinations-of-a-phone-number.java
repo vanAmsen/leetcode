a61757ca4094aef0eeb45b42317b82576f3212c9
Edgar <edgar.amsen@gmail.com>
2023-08-03 03:17:38 +0200

    Create 0017-letter-combinations-of-a-phone-number.java

---
+++ solutions/java/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return Collections.emptyList();
+
+        String[] phone_map = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> output = new ArrayList<>();
+        backtrack("", digits, phone_map, output);
+        return output;
+    }
+
+    private void backtrack(String combination, String next_digits, String[] phone_map, List<String> output) {
+        if (next_digits.isEmpty()) {
+            output.add(combination);
+        } else {
+            String letters = phone_map[next_digits.charAt(0) - '2'];
+            for (char letter : letters.toCharArray()) {
+                backtrack(combination + letter, next_digits.substring(1), phone_map, output);
+            }
+        }
+    }
+}
