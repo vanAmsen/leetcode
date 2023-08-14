d7aa4d077e48fc5c06ea0b6b8ee3cb1232b9a583
Edgar <edgar.amsen@gmail.com>
2023-08-14 04:37:36 +0200

    Create 0215c-kth-largest-element-in-an-array.py

---
+++ solutions/python/0215c-kth-largest-element-in-an-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findKthLargest(self, nums, k):
+        left, right = 0, len(nums) - 1
+        while True:
+            pivot_index = random.randint(left, right)
+            new_pivot_index = self.partition(nums, left, right, pivot_index)
+            if new_pivot_index == len(nums) - k:
+                return nums[new_pivot_index]
+            elif new_pivot_index > len(nums) - k:
+                right = new_pivot_index - 1
+            else:
+                left = new_pivot_index + 1
+
+    def partition(self, nums, left, right, pivot_index):
+        pivot = nums[pivot_index]
+        nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
+        stored_index = left
+        for i in range(left, right):
+            if nums[i] < pivot:
+                nums[i], nums[stored_index] = nums[stored_index], nums[i]
+                stored_index += 1
+        nums[right], nums[stored_index] = nums[stored_index], nums[right]
+        return stored_index
