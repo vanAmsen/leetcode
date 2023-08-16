a6a54350a2c61d862c9816aaa7afb14f38b86ed8
Edgar <edgar.amsen@gmail.com>
2023-08-16 04:51:46 +0200

    Update 0239-sliding-window-maximum.py

--- solutions/python/0239-sliding-window-maximum.py
+++ solutions/python/0239-sliding-window-maximum.py
@@ -1,1 +1,22 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums or k == 0: 
+            return [] 
+        if k == 1: 
+            return nums 
 
+        deq = deque() 
+
+        result = [] 
+        for i in range(len(nums)): 
+            while deq and deq[0] < i - k + 1: 
+                deq.popleft() 
+            while deq and nums[i] > nums[deq[-1]]: 
+                deq.pop() 
+
+            deq.append(i) 
+            if i >= k - 1: 
+                result.append(nums[deq[0]]) 
+
+        return result 
+                                                                                                                                                                                                                             
