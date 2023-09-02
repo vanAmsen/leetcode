dc35bc6103d97fbab60fbcd38b52275ef1cd484b
Edgar <edgar.amsen@gmail.com>
2023-09-02 05:03:07 +0200

    Create 2707-extra-characters-in-a-string.py

---
+++ solutions/python/2707-extra-characters-in-a-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        max_val = len(s) + 1 
+        dp = [max_val] * (len(s) + 1) 
+        dp[0] =0  
+        dictionary_set = set(dictionary) 
+
+        for i in range(1, len(s) + 1): 
+            dp[i] = dp[i - 1] + 1 
+
+            for l in range(1, i + 1):  
+                if s[i-l:i] in dictionary_set: 
+                    dp[i] = min(dp[i], dp[i-l]) 
+
+        return dp[-1] 
+                                                                                                                                          
