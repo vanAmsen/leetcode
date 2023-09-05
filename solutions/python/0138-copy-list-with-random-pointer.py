48b03eaaaac757c87fb443dd287cc18d013d39d5
Edgar <edgar.amsen@gmail.com>
2023-09-05 03:16:58 +0200

    Create 0138-copy-list-with-random-pointer.py

---
+++ solutions/python/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        old_to_new = {}
+        
+        curr = head
+        while curr:
+            old_to_new[curr] = Node(curr.val)
+            curr = curr.next
+        
+        curr = head
+        while curr:
+            old_to_new[curr].next = old_to_new.get(curr.next)
+            old_to_new[curr].random = old_to_new.get(curr.random)
+            curr = curr.next
+            
+        return old_to_new[head]
