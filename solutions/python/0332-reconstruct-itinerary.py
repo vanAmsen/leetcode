6675f52f8aff6b34e97b37e939813e0f653f8e4a
Edgar <edgar.amsen@gmail.com>
2023-09-14 03:03:53 +0200

    Create 0332-reconstruct-itinerary.py

---
+++ solutions/python/0332-reconstruct-itinerary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict(list)
+        
+        for src, dst in sorted(tickets, reverse=True):
+            graph[src].append(dst)
+            
+        itinerary = []
+        def dfs(airport):
+            while graph[airport]:
+                dfs(graph[airport].pop())
+            itinerary.append(airport)
+        
+        dfs("JFK")
+        
+        return itinerary[::-1]
