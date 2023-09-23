102b2acf8754c8f5e431d39d9e8361fdcec64ca8
Edgar <edgar.amsen@gmail.com>
2023-09-23 02:48:28 +0200

    Create 1048-longest-string-chain.py

---
+++ solutions/python/1048-longest-string-chain.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key=len)
+        dp = {}
+        max_chain = 0
+        for word in words:
+            dp[word] = 1
+            for i in range(len(word)):
+                prev_word = word[:i] + word[i+1:]
+                if prev_word in dp:
+                    dp[word] = max(dp[word], dp[prev_word] + 1)
+            max_chain = max(max_chain, dp[word])
+        return max_chain
